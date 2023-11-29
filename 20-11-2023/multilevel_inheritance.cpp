5c483676ccb01d6d1bef13aa88e078cdc5988597
codeshubham <144806483+shubhamcodingblocks@users.noreply.github.com>
2023-11-29 17:15:31 +0530

    late binding, early binding, virtual function, pure virtual function, abstract class, virtual destructors

--- 20-11-2023/multilevel_inheritance.cpp
+++ 20-11-2023/multilevel_inheritance.cpp
@@ -1,32 +1,23 @@
 #include<iostream>
 using namespace std;
 
-class A{
+class B;
+
+class A : public B{
 public:
 	void showA(){
-		cout << "method inside A" << endl;
+		cout << "method in A" << endl;
 	}
 };
 
 class B : public A{
 public:
 	void showB(){
-		cout << "method inside B" << endl;
+		cout << "method in B" << endl;
 	}
 };
-
-class C : public B{
-public:
-	void showC(){
-		cout << "method inside C " << endl;
-	}
-};
-
 
 int main(){
-	C obj;
-	obj.showC();
-	obj.showB();
-	obj.showA();
+	
 	return 0;
 }
